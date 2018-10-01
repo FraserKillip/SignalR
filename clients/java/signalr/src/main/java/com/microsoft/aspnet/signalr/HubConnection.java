a6be4d00b7b9c7e08655a275c2030a46076a32ae
BrennanConroy <brecon@microsoft.com>
2018-10-01 14:31:58 -0700

    rebase

--- clients/java/signalr/src/main/java/com/microsoft/aspnet/signalr/HubConnection.java
+++ clients/java/signalr/src/main/java/com/microsoft/aspnet/signalr/HubConnection.java
@@ -5,6 +5,7 @@ package com.microsoft.aspnet.signalr;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
